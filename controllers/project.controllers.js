2d4b91c6220c9bae0a92f4795646595c6b8bab93
Quoc Hung <hoangtulai107@gmail.com>
2019-05-30 16:14:06 +0700

    delete project

--- controllers/project.controllers.js
+++ controllers/project.controllers.js
@@ -1,6 +1,12 @@
 var db = require('../db') ; 
+var md5 = require('md5') ; 
 module.exports.create = function(req,res){
     res.render('projectCreate') ; 
+}
+module.exports.delete = function(req,res){
+    let temp = res.locals.user ; 
+    let user = db.get('users').find({id : temp.id}).value() ; 
+    res.render('projectDelete',{user}) ; 
 }
 module.exports.project = function(req,res){
     let temp = res.locals.user ; 
@@ -19,4 +25,23 @@ module.exports.postCreate = function(req,res){
     //let temp = db.get('users').find({id : user.id}).value() ; 
     db.get('users').find({id : user.id}).assign(user).write() ; 
     res.redirect('/project') ;
+}
+module.exports.postDelete = function(req,res){
+    let pass = md5(req.body.pass) ; 
+    let choose = req.body.choose ; 
+    //console.log(pass,' ' , choose) ;
+    let temp = res.locals.user ; 
+    let user = db.get('users').find({id : temp.id}).value() ; 
+    if (pass !== user.pass){
+        res.redirect('/project/delete') ; 
+        return ; 
+    }
+    for (let i = 0 ; i < user.projects.length ; i++){
+        if (user.projects[i].name === choose){
+            user.projects.splice(i,1) ; 
+        }
+    }
+    console.log(user) ; 
+    db.get('users').find({id: user.id}).assign(user).write() ; 
+    res.redirect('/project') ; 
 }
--- routes/project.route.js
+++ routes/project.route.js
@@ -4,8 +4,10 @@ var db = require('../db') ;
 var controller = require('../controllers/project.controllers') ;  
 
 router.get('/create',controller.create);
+router.get('/delete',controller.delete) ;  
 router.get('/',controller.project) ; 
 
 
 router.post('/create',controller.postCreate) ; 
+router.post('/delete',controller.postDelete) ; 
 module.exports = router ; 
